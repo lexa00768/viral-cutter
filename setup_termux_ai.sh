0a4e29bebf0bfe357d236244a7ae29973a6d21d7
lexa00768 <73959174+lexa00768@users.noreply.github.com>
2025-06-06 17:20:48 +0300

    Add files via upload

---
+++ setup_termux_ai.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update -y && pkg upgrade -y
+pkg install -y python ffmpeg git wget clang make pkg-config libffi-dev
+
+pip install --upgrade pip setuptools wheel
+pip install yt-dlp
+pip install torch torchvision torchaudio -f https://download.pytorch.org/whl/cpu/torch_stable.html
+pip install git+https://github.com/openai/whisper.git
