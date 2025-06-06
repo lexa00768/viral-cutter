10dea89b4a82069af9b83e57cd6493ed1e38a166
lexa00768 <73959174+lexa00768@users.noreply.github.com>
2025-06-06 17:19:59 +0300

    Delete setup_termux_env.sh

--- setup_termux_env.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-pkg update -y && pkg upgrade -y
-pkg install -y python ffmpeg git wget clang make pkg-config libffi-dev
-
-pip install --upgrade pip
-pip install --no-cache-dir yt-dlp
-pip install --no-deps git+https://github.com/Breakthrough/PySceneDetect.git
